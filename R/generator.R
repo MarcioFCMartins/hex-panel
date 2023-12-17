2f78180c98c8065af77a6ebb2512306626d4f623
MarcioFCMartins <marciomartinsred@gmail.com>
2023-12-17 11:12:47 +0000

    Initialize github repo
    Functions to create hexagonal wall panels based on probability matrices

---
+++ R/generator.R
@@ -0,0 +1,212 @@
+# https://www.redblobgames.com/grids/hexagons/
+# This script creates a hexagonal grid of a given size. Hexagons are drawn in
+# cells selected by binomial draws, with a probability of presence that is
+# modulated by perlin noise and a horizontal logistic gradient.
+
+# Setup -------------------------------------------------------------------
+library(ambient) # Perlin noise generator
+
+# Reverse logistic equation - used to create a gradient of probability
+# which decreases from left to rigth
+# x0 is the x value of the inflection point
+# k is the steepness of the curve
+modified_logistic <- function(x, x0, k) { 
+    y <- 1 / (1 + exp(-k * (x - x0)))
+    # Scale between 1 and -1
+    y <- 2 * (y - min(y))/(max(y) - min(y)) - 1
+    
+    return(y)
+}
+
+# Creates a matrix of hexagons presence/absence per cell
+# Accepts a matrix with presence probabilities and a vector that represents the
+# horizontal gradient values 
+# The gain value increases the probability of presence overall and should be
+# experimented with to generate good looking panels
+create_matrix_binom <- function(noise, gradient, gain = 0.5, seed = 13){
+        
+    for(i in 1:ncol(noise)) {
+        noise[, i] <- noise[, i] + horizontal_grad[i] + gain
+    }
+    
+    noise[noise > 1] <- 1 
+    noise[noise < 0] <- 0
+    
+    cells_to_sim <- noise != 1 & noise != 0
+    
+    set.seed(seed)
+    noise[cells_to_sim] <- rbinom(length(noise[cells_to_sim]), 1, noise[cells_to_sim])
+    
+    return(noise)
+}
+
+# Draw a hexagon (flat face up) at center x and y, with radius r
+draw_hexagon <- function(x, y, r, col = "black", gap = 0, ...) {
+    r <- r - gap
+    polygon(x = x + c(r * cos(0 * pi/ 6),
+                      r * cos(2 * pi / 6), 
+                      r * cos(4 * pi / 6), 
+                      r * cos(6 * pi / 6), 
+                      r * cos(8 * pi / 6), 
+                      r * cos(10 * pi / 6)
+    ),
+    y = y + c(r * sin(0 * pi / 6),
+              r * sin(2 * pi / 6),
+              r * sin(4 * pi / 6),
+              r * sin(6 * pi / 6),
+              r * sin(8 * pi / 6),
+              r * sin(10 * pi / 6)
+    ),
+    col = col, ...)
+}
+
+# Calculate the number of neighbors for a hexagon in position i(x), j(y) in 
+# a matrix representing hexagons in an offset coordinate system
+count_missing_neigbors <- function(hex_mat, i, j) {
+    if(i %% 2 == 0) {
+        # // even cols 
+        # [[+1, +1], [ 0, +1], [ -1, 0], 
+        #     [0, -1], [1, -1,], [+1, 0]],
+        # // odd cols 
+        # [[0, +1,], [-1, +1], [-1, 0,], 
+        #     [-1, -1], [0, -1], [+1, 0]],
+        # even col
+        neighbors <- list(
+            tryCatch(hex_mat[j - 1, i + 1],        
+                     error = function(e) return(1)),
+            tryCatch(hex_mat[j , i + 1],       
+                     error = function(e) return(1)),
+            tryCatch(hex_mat[j + 1, i],        
+                     error = function(e) return(1)),
+            tryCatch(hex_mat[j, i - 1],        
+                     error = function(e) return(1)),
+            tryCatch(hex_mat[j - 1, i - 1],    
+                     error = function(e) return(1)),
+            tryCatch(hex_mat[j - 1, i],    
+                     error = function(e) return(1))
+        )
+    } else {
+        # odd col
+        neighbors <- list(
+            tryCatch(hex_mat[j, i + 1],        
+                     error = function(e) return(1)),
+            tryCatch(hex_mat[j + 1, i + 1],       
+                     error = function(e) return(1)),
+            tryCatch(hex_mat[j + 1, i],        
+                     error = function(e) return(1)),
+            tryCatch(hex_mat[j + 1, i - 1],        
+                     error = function(e) return(1)),
+            tryCatch(hex_mat[j, i - 1],    
+                     error = function(e) return(1)),
+            tryCatch(hex_mat[j - 1, i],    
+                     error = function(e) return(1))
+        )
+    }
+    
+    neighbors <- unlist(neighbors)
+    neighbors <- ifelse(
+        is.na(neighbors),
+        0,
+        neighbors
+    ) 
+    
+    missing_neighbors <- length(neighbors) - sum(neighbors)
+    
+    # Return the number of empty adjacent cells
+    return(missing_neighbors)
+}
+
+
+# Modify the color of a hexagon based on the number of missing neighbors
+define_colors <- function(hex_mat, colors) {
+    color_mat <- matrix(nrow = nrow(hex_mat), ncol = ncol(hex_mat))
+    color_gradient <- colorRamp(colors, space = "Lab")
+    
+    for(i in 1:ncol(hex_mat)) {
+        for(j in 1:nrow(hex_mat)) {
+            missing_neighbors <- count_missing_neigbors(hex_mat, i, j)/6
+            rgb_col <- color_gradient(missing_neighbors)
+            color_mat[j, i] <- rgb(rgb_col[1], rgb_col[2], rgb_col[3], max = 255)
+        }
+    }
+    
+    return(color_mat)
+}
+
+draw_panel <- function(
+        presence_mat, # Matrix of presence/absence probabilities
+        hex_radius,   # Radius for hexagons
+        color = "#60AA20", # Matrix of colors for hexagons, or a single color
+        gap = 1,      # Gap between hexagons
+        missing_neighbors = 2, # How many neighbors must be missing to add a gap
+        coords = F,   # Add coordinates to hexagons?
+        cex           # Size of coordinate text
+){
+    ncols <- ncol(presence_mat)
+    nrows <- nrow(presence_mat)
+    width <- (n_col - 1) * hex_radius * 1.5 + 2 * hex_radius
+    height<- n_row * hex_radius * sqrt(3) + hex_radius * sqrt(3)
+  
+    
+    plot.new()
+    plot.window(
+        xlim = c(0, width), 
+        ylim = c(0, height)
+        )
+        
+    for(i in 1:ncols) {
+        for(j in 1:nrows) {
+            # Offset the y coordinate of every other row
+            offset_y <- ifelse(i %% 2 == 0, 0, sqrt(3) * hex_radius / 2)
+            
+            # Calculate the center of the hexagon
+            center_x <- i * hex_radius * 1.5
+            center_y <- j * sqrt(3) * hex_radius + offset_y
+            
+            # If color is a matrix, use the color in the matrix
+            if(length(color) != 1) {
+                col <- color[j, i]
+            } else {
+                col <- color
+            }
+            
+            # Decide if hexagon should have a gap around it
+            n_missing_neighbors <- count_missing_neigbors(presence_mat, i, j)
+            # If there are more than `missing_neighbors` empty hexagons around, add a gap
+            if(n_missing_neighbors >= missing_neighbors) {
+                gap_this <- gap
+            } else {
+                gap_this <- 0
+            }
+            
+            # Draw the hexagons
+            if(presence_mat[j, i] == 1) {
+                draw_hexagon(
+                    x = center_x,
+                    y = center_y, 
+                    r = hex_radius, 
+                    col = col, 
+                    gap = gap_this)
+            }
+            
+            # Add the height of the center of the hexagon
+            if(coords){
+                text(
+                    x = center_x,
+                    y = center_y,
+                    labels = paste0(
+                        round(center_x),
+                        "\n",
+                        round(center_y),
+                        "\n",
+                        i , "-", j
+                    ),
+                    cex = cex
+                )
+            }
+            
+        }
+    }
+}
+
+
---
+++ README.md
@@ -0,0 +1,133 @@
+# Hexagon wall panel
+Márcio Martins
+
+# Hexagon wall panel
+
+Everyone knows that hexagons are the bestagons. So, why not incorporate
+them in your house decor?
+
+In this project, I document the process of painting a hexagon wall
+panel, from the planning to execution.
+
+Some might call it overkill, but the journey was the destination itself.
+
+## Goal
+
+I want to paint one of my walls green, but not the entire wall. I want
+it to start with solid green and then transition to white. The
+transition will by having an increasing number of white hexagons, until
+it transitions entirely to white.
+
+## Planning the panel
+
+The wall to be painted is 223 cm high and 400 cm wide. I will use
+hexagons with a diameter of 20 cm. Around the green hexagons (i.e. when
+the green is no longer fully solid), there will be a 1 cm white border.
+
+### If you wish to draw a hexagon grid, you must offset their coordinates
+
+I will not go too in depth into this section, but will instead refer you
+to the [red blob games
+post](https://www.redblobgames.com/grids/hexagons/) I used to create the
+functions I will use. It’s a great post, and I highly recommend it.
+
+If you wish to see the code, you can find it in the `generator.R` file.
+
+I want the hexagon panel grids to be automatically generated. I will
+create a matrix of 0s and 1s. Then, I will use this matrix to draw the
+hexagons (0 = white hexagon; 1 = green hexagon)
+
+**To decide if a cell is 0 or 1, I will use random generations from from
+the binomial distribution**. To do so, we need to start with a matrix
+that holds the probability of each cell being colored. Let’s start with
+filling this matrix.
+
+### Creating a coloring probability matrix
+
+First, I will create a matrix to store the probability that a cell is
+green. So, let’s start by deciding the size our matrix:
+
+``` r
+hex_radius <- 10
+hex_gap <- 1
+# Width of the wall section = 300 cm
+n_col <- ceiling(400 / hex_radius)
+# Height of the wall section = 223 cm
+n_row <- floor(223 / hex_radius)
+```
+
+So, our wall will be a `n_row` by `n_col` matrix. I want the wall to
+transition from green to white, as we go from left to right. To do so,
+I’ll use the **logistic function**. The logistic function is defined as:
+
+$$f(x) = \frac{L}{1 + e^{-k(x - x_0)}}$$
+
+Where $L$ is the maximum value, $k$ is the growth rate, and $x_0$ is the
+midpoint. I will use a modified version of the logistic function, where
+$L$ is 1, $k$ is positive, at 0.15, and $x_0$ is 90% of the maximum
+value. This will give me a function that starts at 0, increases slowly,
+and then increases rapidly. We can visualize this as a function of the
+column number (or the distance along the wall):
+
+``` r
+horizontal_grad <- modified_logistic(1:n_col, n_col * 0.9, 0.15)
+plot(horizontal_grad, xlab = "Column", ylab = "Green probability")
+```
+
+![](README_files/figure-commonmark/unnamed-chunk-2-1.png)
+
+Let’s visualize what this would look like as a wall panel:
+
+``` r
+wall <- create_matrix_binom(matrix(data = 0, nrow = n_row, ncol = n_col), horizontal_grad, gain = 0.2)
+
+create_panel(wall, 10)
+```
+
+![](README_files/figure-commonmark/unnamed-chunk-3-1.png)
+
+It’s not bad at all! But I want to add some randomness to it. To do so,
+I will use the **perlin noise** algorithm. This algorithm generates a
+matrix of spatially correlated random numbers. This means that the
+matrix will have some smoothness to it, and there will be “spots” where
+the probability of coloring is higher than others.
+
+``` r
+set.seed(13)
+noise <- noise_perlin(dim = c(n_row, n_col), frequency = 0.2)
+image(t(noise))
+```
+
+![](README_files/figure-commonmark/unnamed-chunk-4-1.png)
+
+In this image you can see lighter areas, where the probability of
+coloring is higher, and darker areas, where the probability of coloring
+is lower. Let’s add this to our wall panel probabilities. We’ll also use
+some “gain” to increase the bias towards green. After all, this is more
+art than science.
+
+``` r
+wall <- create_matrix_binom(noise, horizontal_grad, gain = 0.2)
+
+create_panel(wall, 10)
+```
+
+![](README_files/figure-commonmark/unnamed-chunk-5-1.png)
+
+Now we’re talking! This is a good starting point. We have some white
+hexagons in the mostly solid section, and the green hexagons cluster a
+little more towards the right side. The difference isn’t huge, but
+that’s mostly because this is a “low resolution” panel.
+
+### Creating the panel
+
+I added a hand little option to my function that shows the middle
+coordinates of each hexagon. This will help me set up the panel before
+painting it. Let’s print the panel again, but with coordinates and
+increase the size to make it easier to read.
+
+``` r
+create_panel(wall, 10, coords = T)
+```
+
+![](README_files/figure-commonmark/unnamed-chunk-6-1.png)
